a9a66dd93be7047727a8299df30a001acadc3ffb
Damon Oehlman <damon.oehlman@gmail.com>
2014-03-05 13:08:57 +1100

    A little terser

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ module.exports = function(query, opts) {
   // create the matchme filter
   var matcher = matchme(opts);
 
-  function write(data) {
+  return through(function(data) {
     matcher.target = data;
-    console.log(typeof data);
     if (matcher.query(query).ok) {
       this.queue(data);
     }
-  }
-
-  return through(write);
+  });
 };
